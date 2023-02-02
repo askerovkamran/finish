b94f86e3202d8aac5e1d5740ee98f01cbef91272
kamran <askerari@mail.ru>
2023-02-03 02:41:00 +0300

    Решил задачу под номером 64

---
+++ zadacha64/Program.cs
@@ -0,0 +1,11 @@
+﻿// Задача 64: Выполнить с помощью рекурсии.Задайте значение N. Напишите программу, 
+// которая выведет все натуральные числа в промежутке от N до 1.
+Console.Write("Введите число N: ");
+int N = int.Parse(Console.ReadLine()!);
+
+Console.WriteLine(PrintNumbers(N,1));
+//-----------------Метод------------
+string PrintNumbers(int start, int end){
+    if (start == end) return start.ToString();
+    return (start + " " + PrintNumbers(start - 1, end));
+}
